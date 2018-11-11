48285794d88d6ea42bee3af9afcf85e95622c046
Lucas <lucas_china@sina.com>
2018-11-11 23:37:11 +0800

    补充image分类方法

--- LXYImageCategory/LXYImageCategory/UIImage+LXYCreateImage.h
+++ LXYImageCategory/LXYImageCategory/UIImage+LXYCreateImage.h
@@ -21,6 +21,25 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (UIImage *)createImageWithColor:(UIColor*)color;
 
+
+/** 截取当前image对象rect区域内的图像，坐标是以OrientationLandscapeRight为准的，x与y、width与height均调换*/
+- (UIImage *)subImageWithRect:(CGRect)rect;
+
+/** 压缩图片至指定尺寸 */
+- (UIImage *)rescaleImageToSize:(CGSize)size;
+
+/** 压缩图片至指定像素 */
+- (UIImage *)rescaleImageToPX:(CGFloat )toPX;
+
+/** 在指定的size里面生成一个平铺的图片 */
+- (UIImage *)getTiledImageWithSize:(CGSize)size;
+
+/** UIView转化为UIImage */
++ (UIImage *)imageFromView:(UIView *)view;
+
+/** 将两个图片生成一张图片 */
++ (UIImage*)mergeImage:(UIImage*)firstImage withImage:(UIImage*)secondImage;
+
 @end
 
 NS_ASSUME_NONNULL_END
